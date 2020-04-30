173aa57ab8f56c54602ff0bce5c00c89a6824b52
root <root@ip-172-31-4-0.us-west-1.compute.internal>
2020-04-30 08:09:31 +0000

    new1

---
+++ modules/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-west-1"
+}
+
+module "my_vpc" {
+  source = "https://github.com/Ninad949/new_terraform/"
+  
+    vpc_cidr = "10.0.0.0/16"
+
+  tenancy = "default"
+
+  vpc_id = module.aws_vpc.main.vpc_id
+
+  count_private_subnet = 2
+
+  count_public_subnet = 2
+}
+
